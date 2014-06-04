9d5369d57a6d3c1672caebefc05132b2aa684175
Joseph L <jxrlee@gmail.com>
2014-06-04 14:47:22 -0700

    Auth stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		# handle successful save
+      sign_in @user
+  		redirect_to @user
   	else
   		render 'new'
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  #get 'users/new'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  match '/signup', to: 'users#new', via: 'get'
+  #get 'static_pages/home'
+  match '/signup', 	to: 'users#new', 		via: 'get'
+  match '/signin', 	to: 'sessions#new', 	via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 end
